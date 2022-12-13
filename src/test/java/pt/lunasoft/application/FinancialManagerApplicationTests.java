f839938daeffdd22b7ea5cf9f80ffc4389f00a0f
Leonardo Cantanhêde <leocantanhede@gmail.com>
2022-12-13 17:23:06 +0000

    Commit inicial

---
+++ src/test/java/pt/lunasoft/application/FinancialManagerApplicationTests.java
@@ -0,0 +1,14 @@
+package pt.lunasoft.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
